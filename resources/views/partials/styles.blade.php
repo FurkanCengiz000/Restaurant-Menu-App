e6ccf688ac7af13cef74bf4f01be8bc648e25a6f
Furkan Cengiz <FurkanCengiz000@gmail.com>
2023-03-30 15:05:25 +0300

    Added bootstrap 5 and simple page design

---
+++ resources/views/partials/styles.blade.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Edu+NSW+ACT+Foundation:wght@700&display=swap" rel="stylesheet">
